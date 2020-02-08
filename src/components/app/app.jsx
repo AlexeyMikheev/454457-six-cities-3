ba8b7187cba8246861ffbe55d36fb14381513399
Alexey Miheev <almiheev88@gmail.com>
2020-02-08 10:31:21 +0300

    Update app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {offersCount} = props;
-  // eslint-disable-next-line react/prop-types
-  const {places} = props;
+const App = ({offersCount, places}) => {
   return (
     <Main offersCount={offersCount} places={places} />
   );
