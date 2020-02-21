776965e1a1e5440838a3bfc1be5ef1516b29c4a3
Alexey Miheev <almiheev88@gmail.com>
2020-02-21 16:39:15 +0300

    add offer-detail

---
+++ src/components/offer-detail/offer-detail.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import OfferDetail from "./offer-detail.jsx";
+import {OfferType, FEATURES} from '../../consts.js';
+
+const mock = {
+  id: 1,
+  isPremium: true,
+  cost: 120,
+  isMarked: false,
+  rating: 20,
+  name: `Beautiful & luxurious apartment at great location`,
+  type: OfferType.APARTMENT,
+  image: `img/apartment-01.jpg`,
+  roomsCount: 3,
+  membersCount: 4,
+  images: [
+    `img/room.jpg`,
+    `img/apartment-01.jpg`,
+    `img/apartment-02.jpg`,
+    `img/apartment-03.jpg`,
+    `img/studio-01.jpg`,
+    `img/apartment-01.jpg`,
+  ],
+  features: FEATURES,
+  owner: {
+    name: `Angelina`,
+    avatar: `img/avatar-angelina.jpg`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+    isTrust: true
+  }
+};
+
+it(`Render OfferDetail`, () => {
+
+  const tree = renderer.create(<OfferDetail offer={mock} />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
--- src/components/property/property.jsx
+++ src/components/property/property.jsx
@@ -6,13 +6,13 @@ import {getRatingPercents} from "../../utils.js";
 import Reviews from "../reviews/reviews.jsx";
 import Offers from "../offers/offers.jsx";
 import Map from "../map/map.jsx";
+import OfferDetail from "../offer-detail/offer-detail.jsx";
 
 const property = ({offer, reviews, nearOffers, onPlaceHeaderClick}) => {
-  const {isPremium, cost, isMarked, rating, name, type, roomsCount, membersCount, features, images} = offer;
+  const {images} = offer;
   const {name: ownerName, avatar, description, isTrust} = offer.owner;
 
   const displayImages = images.slice(0, MAX_IMAGES_DISPLAY_COUNT);
-  const ratingPercent = getRatingPercents(rating);
   const displayNearOffers = nearOffers.slice(0, MAX_NEAR_DISPLAY_COUNT);
 
   return (
@@ -29,52 +29,7 @@ const property = ({offer, reviews, nearOffers, onPlaceHeaderClick}) => {
         </div>
         <div className="property__container container">
           <div className="property__wrapper">
-            {isPremium &&
-              <div className="property__mark">
-                <span>Premium</span>
-              </div>
-            }
-            <div className="property__name-wrapper">
-              <h1 className="property__name">
-                {name}
-              </h1>
-              <button className={`property__bookmark-button button ${isMarked ? `place-card__bookmark-button--active` : ``}`} type="button">
-                <svg className="property__bookmark-icon" width="31" height="33">
-                  <use xlinkHref="#icon-bookmark"></use>
-                </svg>
-                <span className="visually-hidden">To bookmarks</span>
-              </button>
-            </div>
-            <div className="property__rating rating">
-              <div className="property__stars rating__stars">
-                <span style={{width: `${ratingPercent}%`}}></span>
-                <span className="visually-hidden">Rating</span>
-              </div>
-              <span className="property__rating-value rating__value">{rating}</span>
-            </div>
-            <ul className="property__features">
-              <li className="property__feature property__feature--entire">
-                {type}
-              </li>
-              <li className="property__feature property__feature--bedrooms">
-                {roomsCount} Bedrooms
-              </li>
-              <li className="property__feature property__feature--adults">
-                Max {membersCount} adults
-              </li>
-            </ul>
-            <div className="property__price">
-              <b className="property__price-value">&euro;{cost}</b>
-              <span className="property__price-text">&nbsp;night</span>
-            </div>
-            <div className="property__inside">
-              <h2 className="property__inside-title">What&apos;s inside</h2>
-              <ul className="property__inside-list">
-                {features.map((feature, i) => (
-                  <li key={feature + i} className="property__inside-item">{feature}</li>
-                ))}
-              </ul>
-            </div>
+            <OfferDetail offer={offer} />
             <div className="property__host">
               <h2 className="property__host-title">Meet the host</h2>
               <div className="property__host-user user">
