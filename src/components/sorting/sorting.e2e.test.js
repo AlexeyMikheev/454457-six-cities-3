df526bcb926d19e79059dfdb53e4059380f86fbc
Alexey Miheev <almiheev88@gmail.com>
2020-02-27 13:43:36 +0300

    Update sorting.e2e.test

--- src/components/sorting/sorting.e2e.test.js
+++ src/components/sorting/sorting.e2e.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import configureStore from "redux-mock-store";
 import {Provider} from "react-redux";
@@ -29,7 +29,7 @@ it(`Should sorting be toggled`, () => {
 
   const onToggleChange = jest.fn();
 
-  const sortingComponent = shallow(<Provider store={store}><Sorting isToggled={false} onToggleChange={onToggleChange}/></Provider>);
+  const sortingComponent = mount(<Provider store={store}><Sorting isToggled={false} onToggleChange={onToggleChange}/></Provider>);
 
   const placesSorting = sortingComponent.find(`span.places__sorting-type`);
 
